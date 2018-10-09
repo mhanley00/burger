507eca7d8d654e3ff639155c6737543b225c069b
mhanley00 <mehanley2@gmail.com>
2018-10-08 23:19:06 -0400

    making sure to add jaws connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,16 @@
 // handy connection.js file and exporting it
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);}
+else {connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
   });
-  
+}; 
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
